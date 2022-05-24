28600b420c2e7083da4248efa8e30ac30e8b5798
LML <2514305019@qq.com>
2022-05-24 13:43:05 +0800

    1.0

---
+++ src/com/git/git.java
@@ -0,0 +1,11 @@
+package com.git;
+
+/**
+ * @author dog-king
+ * @create 2022/5/24
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("版本一");
+    }
+}
